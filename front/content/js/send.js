b3e1a907c75745be0e01df4e11d3c98398f3ce9b
BETHA\gabriel.laurindo <gabriel.laurindo@betha.com.br>
2021-12-03 17:29:26 -0300

    Fixes ecma8

--- front/content/js/send.js
+++ front/content/js/send.js
@@ -1,20 +1,15 @@
-async function send(){
-    try{
-        const nome = document.getElementById("inputNome").value
-        const idade = document.getElementById("inputIdade").value
-        const result = await fetch('https://faculdade-cadastro-pessoa.herokuapp.com',{
-            headers:{
-                "Content-Type": "application/json"
-            },
-            method:"POST",
-            body: JSON.stringify({nome,idade}),
-        });
-        const json = await result.json()
-        window.alert("Registrado com sucesso!")
-     }catch(err){
-         console.log(err)
-     }
-     document.getElementById("inputNome").value = "";
-     document.getElementById("inputIdade").value = "";
-     document.getElementById("gridCheck").checked=false;
+function send(){
+    const nome = document.getElementById("inputNome").value
+    const idade = document.getElementById("inputIdade").value
+    const result = fetch('https://faculdade-cadastro-pessoa.herokuapp.com',{
+        headers:{
+            "Content-Type": "application/json"
+        },
+        method:"POST",
+        body: JSON.stringify({nome,idade}),
+    });
+    window.alert("Registrado com sucesso!");
+    document.getElementById("inputNome").value = "";
+    document.getElementById("inputIdade").value = "";
+    document.getElementById("gridCheck").checked=false;
 }
